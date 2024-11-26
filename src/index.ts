9b84839c20afc96bc67d7555b68ef058f62f9fb9
Amit Kumar <amitkumardhaka25@gmail.com>
2024-11-26 14:52:12 +0530

    CamudaTaskTypes

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as BPMNEditor } from './components/bpmn-diagram/process-bpmn-diagram';
 export { CamundaAPI } from './components/camuda-tasks/index';
+export * as CamudaTaskTypes from './components/camuda-tasks/types';
