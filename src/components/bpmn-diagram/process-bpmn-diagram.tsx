3c5f6d69530cecad44c13a828a8a75571c40ff65
Prem-Kumar-programmer <premprogramer3@gmail.com>
2025-01-30 18:14:01 +0530

    added task Instances count display logic

--- src/components/bpmn-diagram/process-bpmn-diagram.tsx
+++ src/components/bpmn-diagram/process-bpmn-diagram.tsx
@@ -3,10 +3,14 @@ import BpmnModeler from "bpmn-js/lib/Modeler";
 
 const BPMNEditor = ({ 
   bpmnXML, taskDefinitionKeys, taskDefinitionKeyColors,
-  height, width, containerStyle,
+  height, width, containerStyle, taskInstancesCount
 }: { 
   bpmnXML: string; taskDefinitionKeys?: (string | undefined)[], taskDefinitionKeyColors?: string[],
   height?: string | number, width?: string | number, containerStyle?: React.CSSProperties,
+  taskInstancesCount?: {
+    activityId: string | undefined;
+    count: number | undefined;
+}[],
 }) => {
   const modelerRef = useRef<BpmnModeler | null>(null);
   const containerRef = useRef<HTMLDivElement | null>(null);
@@ -28,6 +32,14 @@ const BPMNEditor = ({
             console.log({currentStatusItem: taskDefinitionKey});
             if(!!taskDefinitionKey)
               changeElementBackground(taskDefinitionKey, !!taskDefinitionKeyColors?.length ? taskDefinitionKeyColors[index] : "#90CAF9");
+          })
+        }
+        if (!!taskInstancesCount?.length) {
+          taskInstancesCount.forEach((taskInstance, index) => {
+            console.log({ currentCountItem: taskInstance });
+            if (!!taskInstance?.activityId && taskInstance?.count) {
+              addTaskInstanceOverlay(taskInstance?.activityId, taskInstance?.count);
+            }
           })
         }
       })
@@ -57,7 +69,33 @@ const BPMNEditor = ({
       stroke: "#000000",
     });
   };
+  const addTaskInstanceOverlay = (elementId: string, instanceCount: number) => {
+    if (!modelerRef.current) return;
 
+    const overlays: any = modelerRef.current.get("overlays");
+    const elementRegistry: any = modelerRef.current.get("elementRegistry");
+
+    const element = elementRegistry.get(elementId);
+    if (!element) {
+      console.error(`Element with ID ${elementId} not found for overlays`);
+      return;
+    }
+    overlays.add(element, {
+      position: {
+        bottom: 5, // Shift slightly below the task box
+        left: -5,   // Shift slightly to the left
+      },
+      html: `<div style="
+          background: #70b8db;
+          color: white;
+          font-size: 12px;
+          padding: 2px 8px;
+          border-radius: 10px;
+          font-weight: bold;
+          box-shadow: 0px 2px 4px rgba(0,0,0,0.2);
+        ">${instanceCount}</div>`,
+    });
+  };
   return (
     <div
       ref={containerRef}
